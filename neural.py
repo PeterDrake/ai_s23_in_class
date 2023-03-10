efd8c539b618fc4e095c4654a39521791c43e81d
Peter Drake <drake@lclark.edu>
2023-03-10 10:04:48 -0800

    More work on neural networks.

--- neural.py
+++ neural.py
@@ -2,9 +2,12 @@ import math
 import random
 
 
+LEARNING_RATE = 0.1
+
+
 class InputNeuron:
 
-    def __init__(self, activation):
+    def __init__(self, activation=1):
         self.activation = activation
 
 
@@ -12,9 +15,9 @@ class Neuron:
 
     def __init__(self, previous_layer):
         self.activation = None
-        self.previous_layer = previous_layer
+        self.previous_layer = [InputNeuron()] + previous_layer  # Add bias node
         # Later we'll need a delta value
-        self.weights = [random.gauss(0, 1) for _ in previous_layer]
+        self.weights = [random.gauss(0, 1) for _ in self.previous_layer]
 
     def __repr__(self):
         return str([f'{self.previous_layer[i]} -> {self.weights[i]}' for i in range(len(self.previous_layer))])
@@ -22,6 +25,32 @@ class Neuron:
     def update_activation(self):
         s = sum(self.weights[i] * self.previous_layer[i].activation for i in range(len(self.previous_layer)))
         self.activation = logistic(s)
+
+    def predict(self, inputs):
+        inputs = [1] + inputs
+        for i in range(1, len(inputs)):
+            self.previous_layer[i].activation = inputs[i]
+        self.update_activation()
+        return self.activation
+
+    def train(self, inputs, target):
+        a = self.predict(inputs)
+        t = target
+        delta = -a * (1 - a) * (t - a)
+        for j in range(len(self.previous_layer)):
+            self.weights[j] += -LEARNING_RATE * self.previous_layer[j].activation * delta
+
+
+class Network:
+
+    def __init__(self, sizes):
+        """
+        :param sizes: A list of the number of neurons in each layer, e.g., [2, 2, 1] for a network that can learn XOR.
+        """
+        self.layers = [None] * len(sizes)
+        self.layers[0] = [InputNeuron() for _ in range(sizes[0])]
+        for i in range(1, len(sizes)):
+            self.layers[i] = [Neuron(self.layers[i-1]) for _ in range(sizes[i])]
 
 
 def logistic(x):
@@ -31,9 +60,21 @@ def logistic(x):
     return 1 / (1 + math.exp(-x))
 
 
-n = Neuron([InputNeuron(0.2), InputNeuron(0.7)])
+# n = Neuron([InputNeuron(0), InputNeuron(0)])
+# # n.weights = [-30, 20, 20]
+#
+# inputs = [[0, 0], [0, 1], [1, 0], [1, 1]]
+# targets = [0, 1, 1, 0]
+#
+# for i in inputs:
+#     print(n.predict(i))
+#
+# for _ in range(10000):
+#     for i, t in zip(inputs, targets):
+#         n.train(i, t)
+#
+# print()
+# for i in inputs:
+#     print(n.predict(i))
 
-print(n)
-print(n.activation)
-n.update_activation()
-print(n.activation)
+net = Network([2, 2, 1])
